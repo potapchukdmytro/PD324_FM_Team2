a58b83b3a121ce12667ce64353ba90970bc7a00d
Andro_wp80@student.itstep.org <Andro_wp80@student.itstep.org>
2024-05-14 20:18:46 +0300

    Дизайн головної форми

--- FInancingManager/FInancingManager/Form1.Designer.cs
+++ FInancingManager/FInancingManager/Form1.Designer.cs
@@ -30,14 +30,16 @@
         {
             buttonSignUp = new Button();
             LoginButton = new Button();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // buttonSignUp
             // 
-            buttonSignUp.Location = new Point(296, 228);
-            buttonSignUp.Margin = new Padding(3, 2, 3, 2);
+            buttonSignUp.Location = new Point(531, 61);
             buttonSignUp.Name = "buttonSignUp";
-            buttonSignUp.Size = new Size(82, 22);
+            buttonSignUp.Size = new Size(94, 29);
             buttonSignUp.TabIndex = 0;
             buttonSignUp.Text = "Реєстрація";
             buttonSignUp.UseVisualStyleBackColor = true;
@@ -45,30 +47,56 @@
             // 
             // LoginButton
             // 
-            LoginButton.Location = new Point(159, 228);
+            LoginButton.Location = new Point(528, 13);
+            LoginButton.Margin = new Padding(3, 4, 3, 4);
             LoginButton.Name = "LoginButton";
-            LoginButton.Size = new Size(85, 23);
+            LoginButton.Size = new Size(97, 31);
             LoginButton.TabIndex = 5;
             LoginButton.Text = "Увійти";
             LoginButton.UseVisualStyleBackColor = true;
             LoginButton.Click += LoginButton_Click;
             // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 61);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(452, 293);
+            dataGridView1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(150, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(126, 28);
+            label1.TabIndex = 7;
+            label1.Text = "Ваші доходи";
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(557, 337);
+            ClientSize = new Size(637, 449);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
             Controls.Add(LoginButton);
             Controls.Add(buttonSignUp);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button buttonSignUp;
         private Button LoginButton;
+        private DataGridView dataGridView1;
+        private Label label1;
     }
 }
