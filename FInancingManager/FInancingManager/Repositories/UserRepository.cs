6d31921b1614f8850a9f99902b032801c644845d
misharabeshko <misharabeshko@gmail.com>
2024-05-15 02:38:03 +0300

    Add UserRepository

---
+++ FInancingManager/FInancingManager/Repositories/UserRepository.cs
@@ -0,0 +1,56 @@
+﻿using FInancingManager.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FInancingManager.Repositories
+{
+    public class UserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public void AddUser(UserEntity user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+        public void DeleteUser(int userId)
+        {
+            var user = _context.Users.Find(userId);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+            }
+        }
+
+        public void UpdateUser(UserEntity user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public UserEntity GetUserById(int userId)
+        {
+            return _context.Users.Find(userId);
+        }
+
+        public List<UserEntity> GetAllUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+
+
+    }
+}
